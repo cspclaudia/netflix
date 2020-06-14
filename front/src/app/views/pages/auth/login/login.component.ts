06eb2d702d372a07b50db3f9a85476c4da2d9e53
cspclaudia <cspclaudia@users.noreply.github.com>
2020-06-14 15:30:15 -0300

    validators+perfil

--- front/src/app/views/pages/auth/login/login.component.ts
+++ front/src/app/views/pages/auth/login/login.component.ts
@@ -10,11 +10,12 @@ import { ContaService } from 'src/app/core/services/conta.service';
 })
 export class LoginComponent implements OnInit {
   formLogin: FormGroup;
+  checkboxModel = { value: false };
   constructor(
     private fb: FormBuilder,
     private autenticacao: ContaService,
     private router: Router
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.formLogin = this.fb.group({
@@ -30,10 +31,37 @@ export class LoginComponent implements OnInit {
       .login(this.formLogin.value.email, this.formLogin.value.senha)
       .subscribe((res) => {
         localStorage.setItem('token', res.token);
-        this.router.navigateByUrl('/perfis');
+        this.router.navigateByUrl('perfis');
+        console.log('CHECKBOX: ', this.checkboxModel.value);
+        if (!this.checkboxModel.value) {
+          this.teste();
+        }
       });
   }
   assinar() {
     this.router.navigateByUrl('/Auth/registro');
+  }
+  teste() {
+    var tempoLimite = 4;
+    var tempoInicial = 1;
+    document.onmousemove = document.onkeypress = () => {
+      tempoInicial = 0;
+      console.log('tempo: mousemove', tempoInicial);
+    };
+    window.setInterval(() => {
+      console.log('tempo: setInterval', tempoInicial);
+      if (++tempoInicial >= tempoLimite) {
+        localStorage.removeItem('token');
+        window.location.reload();
+      }
+    }, 3000);
+  }
+  ckeckValue() {
+    if (this.checkboxModel.value == true) {
+      this.checkboxModel.value = false;
+    }
+    else {
+      this.checkboxModel.value = true;
+    }
   }
 }
--- front/src/app/views/pages/auth/registro/registro.component.ts
+++ front/src/app/views/pages/auth/registro/registro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ContaService } from 'src/app/core/services/conta.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-registro',
@@ -10,26 +11,40 @@ import { ContaService } from 'src/app/core/services/conta.service';
 })
 export class RegistroComponent implements OnInit {
   formCadastro: FormGroup;
+  // textMaskTelefone$: BehaviorSubject<{ mask: (RegExp | string)[], guide: boolean }>;
+
   constructor(
     private fb: FormBuilder,
-    private autenticacao: ContaService,
+    private conta: ContaService,
     private router: Router
   ) {}
 
   ngOnInit(): void {
     this.formCadastro = this.fb.group({
-      Telefone: this.fb.control('', [Validators.required]),
       Email: this.fb.control('', [
         Validators.required,
         Validators.pattern('[a-zA-Z0-9-_.]+@[a-zA-Z0-9-_.]+'),
       ]),
-      Senha: this.fb.control('', [Validators.required]),
+      Telefone: this.fb.control('', [
+        Validators.required,     
+        Validators.pattern('[0-9]+'),
+        Validators.minLength(10),
+        Validators.maxLength(11),
+      ]),
+      Senha: this.fb.control('', [
+        Validators.required,
+        Validators.pattern('[a-zA-Z0-9]+'),
+        Validators.minLength(8),
+      ]),
     });
   }
   submit() {
-    this.autenticacao.cadastrarConta(this.formCadastro.value).subscribe((res) => {
+    this.conta.cadastrarConta(this.formCadastro.value).subscribe((res) => {
       console.log(res);
       this.router.navigateByUrl('Auth/login');
     });
+  }
+  assinar() {
+    this.router.navigateByUrl('/Auth/login');
   }
 }
