c847cf2db3a0510eee18a9014875240e426469e8
cspclaudia <cspclaudia@users.noreply.github.com>
2020-06-16 16:43:28 -0300

    tela de videos

--- front/src/app/views/themes/base/base.component.ts
+++ front/src/app/views/themes/base/base.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-base',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BaseComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  video() {
+    this.router.navigateByUrl('/video');
   }
 
 }
